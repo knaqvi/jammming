03b8b05c08a6303afa5aad80ea644dd6bae6c66c
Kashif Naqvi <kashif@Kashifs-MacBook-Pro.local>
2017-11-21 17:48:11 -0800

    jammming 2.2a

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -5,20 +5,10 @@ class Track extends React.Component {
   constructor(props){
     super(props);
     this.addTrack = this.addTrack.bind(this);
-    this.removeTrack = this.removeTrack.bind(this); // Step 54
-  };
-
-  render() {
-    return (
-      <div className="Track">
-        <div className="Track-information">
-          <h3>{this.props.track.name}</h3>
-          <p>{this.props.track.artist} | {this.props.track.album}</p>
-        </div>
-        {this.renderAction};
-      </div>
-    );
+    this.removeTrack = this.removeTrack.bind(this);
   }
+
+
   renderAction() {
 
       if(this.props.isRemoval)
@@ -29,13 +19,27 @@ class Track extends React.Component {
       {
         return (<a className='Track-action' onClick={this.addTrack}>+</a>)
       }
+  }
 
-  }
   addTrack() {
-    this.props.onAdd(this.props.track)
+    this.props.onAdd(this.props.track);
   }
-  removeTrack() { // Step 53
-    this.props.onRemove(this.props.track)
+
+  removeTrack() {
+    // Step 53
+    this.props.onRemove(this.props.track);
+  }
+
+  render() {
+    return (
+      <div className="Track">
+        <div className="Track-information">
+          <h3>{this.props.track.name}</h3>
+          <p>{this.props.track.artist} | {this.props.track.album}</p>
+        </div>
+        {this.renderAction()}
+      </div>
+    )
   }
 }
 export default Track;
